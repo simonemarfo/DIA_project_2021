e2e4e3dc6310c9442bbfd60dda06d1bbc157b548
simonemarfo <simone.marforio@gmail.com>
2021-03-26 10:27:34 +0100

    draft

--- Context.py
+++ Context.py
@@ -15,24 +15,26 @@ class Context():
         self.classes_info = classes_info # load the class information: {'id':0, 'name':'class-1','color':'r'}
         self.n_classes = len(classes_info)
         
-        self.item1_prices = item1_prices # candidates price for first item 
+        self.item1_prices = item1_prices # candidates price for first item: [pr0, pr1, pr2, pr3]
         self.item2_prices = item2_prices # candidates price for second item
-        self.item1_probabilities = item1_probabilities
+        self.item1_probabilities = item1_probabilities # item1 seasonal probabilities: [{'id':0,'name':'season1', 'probabilities':[[]], {...}]
         self.item2_probabilities = item2_probabilities 
         
 
     def plot_conversion_rate(self,prices,probabilities, title='Conversion rate'):
+        fig, axs = plt.subplots(3,constrained_layout=True)
+        fig.suptitle(title)
+        for season, ax in enumerate(axs):
+            for c in self.classes_info:
+                idx = c['id']
+                f_x, f_y = interpolate(np.array(prices), np.array(probabilities[season]['probabilities'][idx]))
+                ax.plot(f_x,f_y,color=c['color'],label=c['name'])
+                ax.scatter(prices,probabilities[season]['probabilities'][idx],marker='o',color=c['color'])
+                ax.legend()
+                ax.set_title(probabilities[season]['name'])
+                ax.set_xlabel("price")
+                ax.set_ylabel("probabiliy")
 
-        for c in self.classes_info:
-            idx = c['id']
-            f_x, f_y = interpolate(np.array(prices), np.array(probabilities[idx]))
-            plt.plot(f_x,f_y,color=c['color'],label=c['name'])
-            plt.scatter(prices,probabilities[idx],marker='o',color=c['color'])
-
-        plt.xlabel("price")
-        plt.ylabel("probabiliy")
-        plt.title(title)
-        plt.legend()
         plt.show()
     
     def plot_item1_conversion_rate(self):
