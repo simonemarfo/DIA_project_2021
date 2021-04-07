96571f46d74041e91ac7f9a8bac58c2f7e892088
simonemarfo <simone.marforio@gmail.com>
2021-04-07 18:24:50 +0200

    fix experiment 2

--- n2.py
+++ n2.py
@@ -3,98 +3,112 @@ import matplotlib.pyplot as plt
 from Algorithms.UCB_Matching import *
 
 ctx= Context()
-n_exp = 1
-days = 10
+
+days = 365 # 365 days of simulations
 
 item1_price_full = 2350.0
 item2_price_full = 630.0 
 
-#Matrix conversion rate second item
-
+#discount for the second item 
 discounted_price = [item2_price_full,
     item2_price_full*(1-ctx.discount_promos[1]),
     item2_price_full*(1-ctx.discount_promos[2]),
     item2_price_full*(1-ctx.discount_promos[3])]
 
-# Matching matrix: rows[0..3] are the user categories; columns[0..3] are the discouts; celles are the conversion rate of that class
+print("\n\n#############\n")
+print(f" {ctx.items_info[0]['name']}: {item1_price_full} €\n {ctx.items_info[1]['name']}: {item2_price_full} €\n Discouts (%): {[_*100 for _ in ctx.discount_promos]}")
+print(f" Discounted {ctx.items_info[1]['name']}: {discounted_price} €")
+
+# Matrix conversion rate of second item: rows[0..3] are the user categories; columns[0..3] are the discouts; celles are the conversion rate of that class
 conversion_rate_second = np.zeros((4,4))
 for i in range (0,4): #classes
     for j in range (0,4): #promos
         conversion_rate_second[i,j] = (ctx.conversion_rate_second_element(discounted_price[j], i))
 
+# Computing an optimal solution to be compared with the online solutions
+# Is computed using a matching algorithom on a matrix that takes into account the price and conversion rate for the second items, according to the user category and the discount
 priced_conversion_rate_second = np.zeros((4,4))
-opt_rew = []
 for i in range (0,4): #classes
     for j in range (0,4): #promos
-        priced_conversion_rate_second[i,j] =conversion_rate_second[i,j] * discounted_price[j]
-opt=linear_sum_assignment(priced_conversion_rate_second, maximize=True)
-period_reward = []
+        priced_conversion_rate_second[i,j] = conversion_rate_second[i,j] * discounted_price[j]
+opt = linear_sum_assignment(priced_conversion_rate_second, maximize=True) # optimal solution row_ind, col_ind
 
-for e in range(n_exp):
-    learner= UCB_Matching(conversion_rate_second.size, *conversion_rate_second.shape) #Inizializzazione learner UCB
-    #print(e)
-    rew_UCB=[]
+#
+# ONLINE LEARNING AND SIMULATION
+#
 
+period_UCB_reward = [] # rewards collected in a period (days) performing the online learning strategy
+period_opt_reward = [] # rewards collected in a period (days) performing the online learning strategy
 
-    for t in range(days):
-        #Nuova giornata
-        #1. Generazione clienti e distribuzione per le varie classi
-        print(f"DAY: {t}")
-        daily_customer = ctx.customers_daily_instance()
-        #daily_customer= [605, 776, 2131, 1210]
-        print(f"DISTRIBUTION CUSTOMER : {daily_customer}")
-        cum_rewards = 0
-        cum_opt_reward = 0
-        
-        for category in range(len(daily_customer)):
-            for customer in range(daily_customer[category]): # for each category emulate the user that purchase the good
-                # buy first item 
-                #2. Simulazione acquisto primo elemento, senza ottimizzazioni
-                reward_item1 = ctx.purchase_online_first_element(item1_price_full,category) * item1_price_full
-                cum_rewards += reward_item1
-                cum_opt_reward += reward_item1
-                #3. Se acquista il primo elemento, chiamata bandit 
-                if (reward_item1 > 0):
-                    #4. Chiediamo al learner il matching per l'ottimizzazione promo-classi + #5. Restituzione righe,colonne con max reward
-                    sub_matching = learner.pull_arm()
-                    #6. Simulazione acquisto tramite Context solo sulla classe-promo suggerita                    
-                    reward_item2 = ctx.purchase_online_second_element(discounted_price[sub_matching[1][category]],category) 
-                    cum_rewards += (reward_item2* discounted_price[sub_matching[1][category]])
-                    rewards_to_update = np.zeros ((4))
-                    rewards_to_update[category] = reward_item2
-                    learner.update(sub_matching,rewards_to_update)
-                    cum_opt_reward += (ctx.purchase_online_second_element(discounted_price[opt[1][category]],category) * discounted_price [opt[1][category]])
-        
-        opt_rew.append(cum_opt_reward)
-        period_reward.append(cum_rewards)
+learner = UCB_Matching(conversion_rate_second.size, *conversion_rate_second.shape) # Initialize UCB matching learner
+
+for t in range(days): # Day simulation
+    # 1. Generate daily customers according the Context distributions, divided in categories
+    daily_customer = ctx.customers_daily_instance()
+    cum_UCB_rewards = 0
+    cum_opt_rewards = 0
     
-        print(f"DAILY REWARD CUMULATIVE: {cum_rewards}")
-        print("CONFIDENCE")
-        print(learner.confidence)
-    print(f"Period Reward: {period_reward}")
-    print(f"Optimal Reward: {opt_rew}")
+    for category in range(len(daily_customer)):
+        for customer in range(daily_customer[category]): # for each category emulate the user that purchase the good 
+
+            #2. Purchase simulation of the first element. (no optimization strategy)
+            reward_item1 = ctx.purchase_online_first_element(item1_price_full,category) * item1_price_full
+            cum_UCB_rewards += reward_item1
+            cum_opt_rewards += reward_item1
+
+            #3. Propose the second item only if the first one was bought
+            if (reward_item1 > 0):
+                #4. Query the learner to know wath is the best matching strategy category-promotion 
+                sub_matching = learner.pull_arm() # suboptimal matching. row_ind, col_ind
+
+                #5. Propose the second item to the user, using the promotion that retrieved by the learner (according to the user category)                    
+                reward_item2 = ctx.purchase_online_second_element(discounted_price[sub_matching[1][category]],category) # 0: not purchased, 1: purchased
+
+                #6. update the learner according to the obtained reward. rewards_to_update is a 4-zeros array, except for the element representing the current user category that contain the obtained reward
+                rewards_to_update = np.zeros ((4))
+                rewards_to_update[category] = reward_item2 
+                learner.update(sub_matching,rewards_to_update)
+
+                # store results in the cumulative daily rewards 
+                cum_UCB_rewards += (reward_item2* discounted_price[sub_matching[1][category]])
+                cum_opt_rewards += (ctx.purchase_online_second_element(discounted_price[opt[1][category]],category) * discounted_price [opt[1][category]]) # simulate the purchase of the second item according to the optimal strategy
+
+    period_UCB_reward.append(cum_UCB_rewards)
+    period_opt_reward.append(cum_opt_rewards)
+    
+    print('___________________')
+    print(f'| Day: {t+1}')
+    print(f'| Today customers distribution : {daily_customer}')
+    print(f'| Today cumulative reward (Online strategy): {cum_UCB_rewards}\n| Today cumulative reward (Optimal strategy): {cum_opt_rewards}\n| - Loss: {cum_opt_rewards - cum_UCB_rewards}')
+    print(f'Current confidence per arm of the online learner:\n{learner.confidence}')
+    print('___________________\n')
+
+# print(f"Period UCB Reward: {period_UCB_reward}")
+# print(f"Period Optimal Reward: {period_opt_reward}")
    
-    mean_reward=np.mean(period_reward)
-    print(f"MEAN REWARD 365d: {mean_reward}")
-        
-                    
-                
+mean_UCB_reward = np.mean(period_UCB_reward)
+mean_opt_reward = np.mean(period_opt_reward)
+print(f"Mean daily reward using online UCB strategy: {mean_UCB_reward}")
+print(f"Mean daily reward using optimal strategy: {mean_opt_reward}")
+print(f'Period ({days} days) regret: {np.sum(period_opt_reward) - np.sum(period_UCB_reward)}')
 
-    #print(f"Cumulative rewards {cum_rewards}")
-                #7. Passare al Learner il vettore di reward partizionato alla classe --> learner.update(pulled_arms,[0,rewards[1],0])
-                #8. Salvare i risultati
-                #9. Torna al punto 2 per gli altri clienti
-
-
-#2410829.88
-"""
-        learner.update(pulled_arms,rewards)
-        rew_UCB.append(rewards.sum())
-        opt_rew.append(p[opt].sum())
-    regret_ucb[e,:]=np.cumsum(opt_rew)-np.cumsum(rew_UCB)
-    print(learner.confidence)
+# plot daily reward comparison
 plt.figure(0)
-plt.plot(regret_ucb.mean(axis=0))
+plt.xlabel("day")
+plt.ylabel("Daily reward")
+plt.plot(period_UCB_reward,'-o', color='red', label = 'UCB Strategy')
+plt.plot(days * [mean_UCB_reward],'--', color='lightcoral', label = 'Mean UCB Strategy')
+plt.plot(period_opt_reward,'-o', color='blue', label = 'Optimal Strategy')
+plt.plot(days * [mean_opt_reward],'--', color='cornflowerblue', label = 'Mean Optimal Strategy')
+plt.legend()
+
+# plot regret of UCB
+
+plt.figure(1)
+plt.plot(np.cumsum(period_opt_reward) - np.cumsum(period_UCB_reward))
 plt.ylabel('Regret')
-plt.xlabel('t')
-plt.show()"""
+plt.xlabel('day')
+
+plt.show()
+        
+    
